87e81183be090b38a7aba29b900288e77107de71
naysantos520 <108131476+naysantos520@users.noreply.github.com>
2024-09-25 09:57:58 -0300

    Add files via upload

---
+++ graficos/informacoesgloblais.js
@@ -0,0 +1,12 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log()
+    const paragrafo document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${total_pessoas_conectadas} estao`
+}
+
+vizualizarInformacoesGlobais()
